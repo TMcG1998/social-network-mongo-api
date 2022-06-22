34294c6c551bf88afe506d68d79f9b0d21bf89e9
Tommy McG <thomasjmcgovern98@gmail.com>
2022-06-22 18:26:57 -0400

    pseudo-coded controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userController = {
+    // -> /api/users
+    // get all users
+
+    // get a single user by it's id and populated thought and friend data
+
+    // post a new user
+
+    // put to update a user by its id
+
+    // delete to remove a user by its id
+
+    // -> /api/users/:userId/friends/:friendId
+    // post to add a new friend to a users friend list
+
+    // delete to remove a friend from users friend list
+    
+}
+
+module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
+// import all functions from user-controller
+
 
 module.exports = router;
