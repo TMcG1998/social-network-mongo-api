220791a3e32ea8f8bc8db3eb4db89cc70d85e77f
Tommy McG <thomasjmcgovern98@gmail.com>
2022-06-23 20:59:06 -0400

    added get all, get by id and create to thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,12 +1,57 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 const thoughtController = {
     // -> /api/thoughts
     // GET to get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .select('-__v')
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
+    },
 
     // GET to get a single thought by its _id
+    getThoughtById( { params }, res) {
+        Thought.findOne( {_id: params.id} )
+        .select('-__v')
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({ message: 'No user found with this id!' })
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
+    },
 
     // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            )
+        })
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({ message: 'No user found with this id!' })
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
+    },
 
     // PUT to update a thought by its _id
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought
+} = require('../../controllers/thought-controller');
+
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router.route('/:id')
+    .get(getThoughtById);
+
 module.exports = router;
